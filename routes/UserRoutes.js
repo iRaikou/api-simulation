42af29d3bf34e877326f38bd511b34a619e7bba2
iRaikou <50130268+iRaikou@users.noreply.github.com>
2025-09-04 21:00:56 +0800

    add get user details

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -147,4 +147,20 @@ router.post("/check-credentials", async (req, res) => {
   }
 });
 
+router.get("/get-user/:email", async (req, res) => {
+  try {
+    const { email } = req.params;
+
+    // Find user by email
+    const user = await User.findOne({ email }).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 export default router;
